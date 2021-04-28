1b6a053af73779aaefd37cfeaee33d6387d2d5e7
TsenHill <chou870808@gmail.com>
2021-04-28 15:55:18 +0800

    first commit

---
+++ Debug/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,29 @@
+FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
+ ../FreeRTOS/portable/GCC/ARM_CM4F/port.c \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/FreeRTOS.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/projdefs.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/portable.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/deprecated_definitions.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/mpu_wrappers.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/task.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/list.h
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/FreeRTOS.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/FreeRTOSConfig.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/projdefs.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/portable.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/deprecated_definitions.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/mpu_wrappers.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/task.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091234/FreeRTOS/include/list.h:
